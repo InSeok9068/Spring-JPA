7980f152b73a31119cc757093853dbeae5fb057b
SANHAIT <dlstjr9068@naver.com>
2019-03-25 16:34:05 +0900

    ㅇㅇ

--- src/main/java/com/jojoldu/jpaservice/domain/AcademyRepository.java
+++ src/main/java/com/jojoldu/jpaservice/domain/AcademyRepository.java
@@ -8,9 +8,9 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface AcademyRepository extends JpaRepository<Academy, Long>{
 
-	/**
-     * 1. join fetch를 통한 조회
-     */
+    /**
+    * 1. join fetch를 통한 조회
+    */
     @Query("select a from Academy a join fetch a.subjects")
     List<Academy> findAllJoinFetch();
 
